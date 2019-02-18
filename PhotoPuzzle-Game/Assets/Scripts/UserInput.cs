ec0f69e641864b921fb1f536c6f95fe0847de5f1
Keith Connors <c17507803@mydit.ie>
2019-02-18 22:57:37 +0000

    .

---
+++ PhotoPuzzle-Game/Assets/Scripts/UserInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour
+{
+    public GameObject VideoCamera;
+    public Component MainCam;
+    private void Start()
+    {
+        MainCam = GetComponentInChildren<Camera>();
+        VideoCamera.active = false;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.N))
+        {
+            if(VideoCamera.active == false)
+            {
+                MainCam.GetComponent<Camera>().enabled = false;
+                VideoCamera.active = true;
+            }
+            else
+            {
+                MainCam.GetComponent<Camera>().enabled = true;
+                VideoCamera.active = false;
+            }
+        }
+    }
+}
