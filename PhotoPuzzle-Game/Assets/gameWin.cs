f341caf58d934868455fe43eb4b711315f3a260a
dinmaker <37370379+dinmaker@users.noreply.github.com>
2019-05-16 19:54:08 +0100

    puzzles implemented on level 2 scene

---
+++ PhotoPuzzle-Game/Assets/gameWin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameWin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Application.Quit();
+        }
+    }
+}
