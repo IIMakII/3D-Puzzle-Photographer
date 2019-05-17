62710ee2bf655377a3fb318fcb905fe72cd4ae7f
dinmaker <37370379+dinmaker@users.noreply.github.com>
2019-05-17 21:13:39 +0100

    gave finished screen

--- PhotoPuzzle-Game/Assets/Scripts/Game End.cs
+++ PhotoPuzzle-Game/Assets/Scripts/Game End.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class gameWin : MonoBehaviour
+public class GameEnd : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void ReturnToMenu()
+    void Start()
     {
-        SceneManager.LoadScene("Start screen");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
