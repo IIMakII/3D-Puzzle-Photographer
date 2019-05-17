35ba32663d87172236fcbcd1e9d7c07180d96f3c
dinmaker <37370379+dinmaker@users.noreply.github.com>
2019-05-17 01:30:30 +0100

    Revert "fixing?"
    This reverts commit 283da4215fa8a43b54575b4a92993d1ed26b9aef.

--- PhotoPuzzle-Game/Assets/Scripts/gameWin.cs
+++ PhotoPuzzle-Game/Assets/Scripts/gameWin.cs
@@ -9,16 +9,7 @@ public class gameWin : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            if (Application.isEditor)
-            {
-#if UNITY_EDITOR
-                UnityEditor.EditorApplication.isPlaying = false;
-#endif
-            }
-            else
-            {
-                Application.Quit();
-            }
+            Application.Quit();
         }
     }
 }
