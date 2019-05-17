a5575733128c5ae1664536057df45bd961c1dbf1
Keith Connors <c17507803@mydit.ie>
2019-05-17 14:49:48 +0100

    Dialogues - Intro - Triggers

---
+++ PhotoPuzzle-Game/Assets/Scripts/ShowPopupTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowPopupTrigger : MonoBehaviour
+{
+
+public Text PopupText;    
+public float destroyTimer;
+
+    void Start()
+    {
+        PopupText.GetComponent<Text>().enabled = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            PopupText.GetComponent<Text>().enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            PopupText.GetComponent<Text>().enabled = false;
+            Debug.Log("Destorying "+this.gameObject.name+" in "+destroyTimer+" seconds.");
+            Destroy(gameObject, destroyTimer);
+        }
+    }
+}
